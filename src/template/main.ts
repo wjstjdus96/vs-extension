d4c5643290ae7dbc1b88683a8ee0f0bc00aa46c2
seoyeon <wjstjdus96@naver.com>
2024-03-18 19:31:46 +0900

    fix: 웹팩 설정 및 main.ts 파일 수정

--- src/template/main.ts
+++ src/template/main.ts
@@ -2,7 +2,7 @@ export const mainTemplate = (name: string, stylePath: string) => {
   return `import * as S from "./${stylePath}";
 
 export const ${name} = () => {
-    return <div>${name} 컴포넌트</div>
+    return <div>${name}</div>
 }
 `;
 };
